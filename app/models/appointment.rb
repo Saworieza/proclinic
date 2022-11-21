7538aba87097caed657da4a5f8004a1db069a9f6
Saworieza <saworieza@gmail.com>
2022-11-21 14:09:22 +0300

    worked on payments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,6 +6,8 @@ class Appointment < ApplicationRecord
   belongs_to :department
   belongs_to :user
 
+  has_many :payments
+
   def set_token
     self.update_attribute(:token, Time.now.strftime("%Y")+ '/' + ("%.3i" %self.id))
   end
